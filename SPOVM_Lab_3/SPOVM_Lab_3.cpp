0b5cdf7161985c8dd4f8b8d404307f07c5a22752
Алексей Григарович <61623090+Alexei-Grigarovich@users.noreply.github.com>
2020-04-26 16:09:45 +0300

    Add files via upload

---
+++ SPOVM_Lab_3/SPOVM_Lab_3.cpp
@@ -0,0 +1,83 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <locale.h>
+#include <windows.h>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+LPWSTR CharToLPWSTR(LPCSTR char_string)
+{
+	LPWSTR res;
+	DWORD res_len = MultiByteToWideChar(1251, 0, char_string, -1, NULL, 0);
+	res = (LPWSTR)GlobalAlloc(GPTR, (res_len + 1) * sizeof(WCHAR));
+	MultiByteToWideChar(1251, 0, char_string, -1, res, res_len);
+	return res;
+}
+
+void main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "ru");
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	if (argc == 1) {
+		string message;
+
+		STARTUPINFO info;
+		PROCESS_INFORMATION processInfo;
+
+		ZeroMemory(&info, sizeof(info));
+		ZeroMemory(&processInfo, sizeof(processInfo));
+
+		HANDLE Work = CreateSemaphore(NULL, 0, 1, L"Work");
+		HANDLE WINAPI fileMapping = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 1, L"FileProjection");
+
+		LPVOID mapView = MapViewOfFile(fileMapping, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+
+		if (!CreateProcess(CharToLPWSTR(argv[0]), (LPWSTR)L" child", NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &info, &processInfo)) {
+			cout << "Process start error" << endl;
+			system("pause");
+			exit(0);
+		}
+
+		cout << "Введите сообщение клиенту" << endl;
+		while (true) {
+			getline(cin, message);
+
+			strcpy((char*)mapView, const_cast<char*>(message.c_str()));
+
+			ReleaseSemaphore(Work, 1, NULL);
+			WaitForSingleObject(Work, INFINITE);
+
+			if (message == "exit") {
+				TerminateProcess(processInfo.hProcess, 0);
+				CloseHandle(fileMapping);
+				CloseHandle(Work);
+				CloseHandle(processInfo.hProcess);
+				CloseHandle(processInfo.hThread);
+				UnmapViewOfFile(mapView);
+				exit(0);
+			}
+		}
+	}
+	else {
+		HANDLE Work = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"Work");
+		HANDLE WINAPI fileMapping = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, L"FileProjection");
+
+		LPVOID mapView = MapViewOfFile(fileMapping, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+
+		cout << "Клиент:" << endl;
+
+		while (true)
+		{
+			WaitForSingleObject(Work, INFINITE);
+
+			cout << "Получено сообщение от сервера: " << (char*)mapView << endl;
+
+			ReleaseSemaphore(Work, 1, NULL);
+		}
+	}
+}
---
+++ SPOVM_Lab_4/SPOVM_Lab_4.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <locale>
+#include <cstdlib>
+#include <conio.h>
+#include <Windows.h>
+#include <string>
+#include <atlstr.h>
+#include <vector>
+
+using namespace std;
+
+CRITICAL_SECTION section;
+
+void printProcess(int p) {
+	cout << "П";
+	Sleep(100);
+	cout << "р";
+	Sleep(100);
+	cout << "о";
+	Sleep(100);
+	cout << "ц";
+	Sleep(100);
+	cout << "е";
+	Sleep(100);
+	cout << "с";
+	Sleep(100);
+	cout << "c";
+	Sleep(100);
+	cout << " " << p << endl;
+}
+
+DWORD WINAPI threadFun(LPVOID lpParam) {
+	int num = *(int*)lpParam;
+	while (true) {
+		while (TryEnterCriticalSection(&section) == 0) {}
+		printProcess(num);
+		LeaveCriticalSection(&section);
+		Sleep(10);
+	}
+}
+
+inline void close(vector<HANDLE> hndls) {
+	for (int i = 0; i < hndls.size(); i++) {
+		TerminateThread(hndls[i], 0);
+	}
+	DeleteCriticalSection(&section);
+	exit(0);
+}
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "ru");
+
+	vector<HANDLE> hndls;
+
+	HANDLE thread;
+
+	InitializeCriticalSection(&section);
+
+	int* num = new int;
+	*num = 0;
+
+	while (true) {
+		if (_kbhit()) {
+			switch (_getch())
+			{
+			case '+':
+				while (TryEnterCriticalSection(&section) == 0) {}
+
+				thread = CreateThread(NULL, 0, threadFun, (LPVOID)num, 0, NULL);
+
+				if (thread == NULL) {
+					cout << "Error create thread" << endl;
+					continue;
+				}
+
+				(*num)++;
+
+				hndls.push_back(thread);
+
+				LeaveCriticalSection(&section);
+
+				break;
+			case '-':
+				if (!hndls.empty()) {
+					while (TryEnterCriticalSection(&section) == 0) {}
+					TerminateThread(hndls[hndls.size() - 1], 0);
+					hndls.pop_back();
+					(*num)--;
+					LeaveCriticalSection(&section);
+				}
+				break;
+			case 'q':
+				close(hndls);
+				break;
+			case 169:
+				close(hndls);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
